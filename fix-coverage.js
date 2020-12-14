758df5df6f21d176888483bc4d3b50be3ed67c5e
Arek Mytych <amytych@gmail.com>
2020-12-14 16:26:44 +0100

    Remove path to missing file

--- fix-coverage.js
+++ fix-coverage.js
@@ -1,13 +1,12 @@
-var fs = require("fs"),    
+var fs = require("fs"),
     prefix = "var __decorate =",
     paths = [
         "built/src/promisedComputed.js",
-        "built/src/deprecatedComputedAsync.js",
     ];
 
 paths.forEach(path => {
 
-    // tell istanbul to ignore TS-generated decorator code 
+    // tell istanbul to ignore TS-generated decorator code
     var src = fs.readFileSync(path, "utf8");
     src = src.replace(prefix, "/* istanbul ignore next */\n" + prefix);
     fs.writeFileSync(path, src);
