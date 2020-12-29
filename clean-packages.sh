b97fa6ed54ef55849afc06bc00e8732ff82ee309
Daniel Earwicker <dan@earwicker.com>
2020-12-29 00:34:33 +0000

    More cleaned

--- clean-packages.sh
+++ clean-packages.sh
@@ -4,4 +4,6 @@ rm -rf packages/computed-async-mobx/node_modules
 rm -rf packages/computed-async-mobx/coverage
 rm -rf packages/computed-async-mobx/built
 rm -rf packages/computed-async-mobx-interactive-tests/node_modules
+rm -rf packages/computed-async-mobx-interactive-tests/.cache
+rm -rf packages/computed-async-mobx-interactive-tests/dist
 rm yarn.lock
